2dbc9d8fad09a673e6f52acf2f5df50a06489db1
Kieran Kelly <kieranjokelly@gmail.com>
2026-01-21 09:52:19 -0500

    Refactor code structure for improved readability and maintainability

--- random/divideandconquer.cpp
+++ random/divideandconquer.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 //inputs a sorted vector and a target value
-bool binarySearch(const vector<int>& arr, int target, int left, int right) {
+bool divideandConquer(const vector<int>& arr, int target, int left, int right) {
     if (left > right) {
         return false; // base is target no can findy
     }
@@ -11,8 +11,21 @@ bool binarySearch(const vector<int>& arr, int target, int left, int right) {
     if (arr[mid] == target) {
         return true; // target foundy
     } else if (arr[mid] > target) {
-        return binarySearch(arr, target, left, mid - 1); // search left 
+        return divideandConquer(arr, target, left, mid - 1); // search left 
     } else {
-        return binarySearch(arr, target, mid + 1, right); // search right
+        return divideandConquer(arr, target, mid + 1, right); // search right
     }
+}
+
+int main() {
+    vector<int> sortedArray = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+    int target = 7;
+
+    if (divideandConquer(sortedArray, target, 0, sortedArray.size() - 1)) { // bc it needs to just search the array minus the one
+        cout << "Found " << target << " in the array" << endl;
+    } else {
+        cout << "Couldn't find  " << target << "  in the array" << endl;
+    }
+
+    return 0;
 }
